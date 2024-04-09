f187a7c27b0192e7a7ef46d0793d3720d3ac337e
DANIEL VALDEZ GUZMAN, BHD <daniel_valdezguzman@bhd.com.do>
2024-04-09 13:10:45 -0400

    init repo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>ApiUtils.js</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/devdanielvaldez/apiutils',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://join.slack.com/t/darkinnovations/shared_invite/zt-2ggv4exit-ft3vguEUJxkXK6Vg2HqENg',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/devdanielvaldez/apiutils',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'ApiUtils.js Doc',
   },
 }
 
