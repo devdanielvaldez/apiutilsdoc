114c7e70b8d77380ef35ae160a825c1d33b6b2ac
DANIEL VALDEZ GUZMAN, BHD <daniel_valdezguzman@bhd.com.do>
2024-04-09 13:19:32 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  output: "export",
-  reactStrictMode: true,
 })
 
 module.exports = withNextra()
